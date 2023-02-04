707c84284d90422f61949896e4e643811c60c14c
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-02-04 11:00:00 +0900

    Add search condition setting process

--- src/process/bot/run.py
+++ src/process/bot/run.py
@@ -1,13 +1,18 @@
 import traceback
 
-from process.bot.scraping_classes.hoge import scraping_classes
+from process.bot.scraping.scraper import Scraper
 
 
+# TODO:Select処理に必要な引数をどのように持ってくるか別途考える
 def run_bot(default_dl_path):
+    url = "https://suumo.jp/"
     try:
-        with scraping_classes(default_dl_path=default_dl_path) as bot:
+        with Scraper(default_dl_path=default_dl_path) as bot:
             bot.land_first_page(url)
-            bot.login_page(id, password)
+            bot.select_region(target_region="")
+            bot.select_real_estate(target_real_estate="")
+            bot.select_area_or_line(target_map="", way="")
+            bot.select_city(city_codes="")
 
         return "success"
 
