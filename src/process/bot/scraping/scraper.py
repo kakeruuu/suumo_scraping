754b503fcb37854dc481542cc6d8847c1e56deb1
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-02-22 21:05:16 +0900

    Fix processing to BS4.

--- src/process/bot/scraping/scraper.py
+++ src/process/bot/scraping/scraper.py
@@ -2,6 +2,9 @@ import pdb
 import time
 import traceback
 
+import pandas as pd
+from bs4 import BeautifulSoup
+from bs4.element import Tag
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
@@ -51,42 +54,40 @@ class Scraper(webdriver.Chrome):
         select_conditions.select_main_conditions(conditions.main_conditions)
         select_conditions.select_other_conditions(conditions.other_condtions)
 
+    # bs4 base
     def manage_scrape_property_list(self):
         try:
             pdb.set_trace()
-            easy_db = DataFrames()
-            property_li = self.find_elements(By.CSS_SELECTOR, "[class='cassetteitem']")
-
+            all_data = []
             start = time.perf_counter()
+            soup = BeautifulSoup(self.page_source, "lxml")
+
+            # 物件リストを取得
+            property_li = soup.select("[class='cassetteitem']")
+
+            # 物件リストから一つの要素を取得
+
             for li in property_li:
-                # 物件詳細の取得
+
                 property_data = self.obtain_property_info(li)
 
-                # 住戸情報の取得
                 dwelling_unit_data = self.obtain_dwelling_unit(li)
 
-                # 住戸情報と物件詳細の結合
                 data = [property_data + r for r in dwelling_unit_data]
 
-                easy_db.add_df(data)
+                all_data.extend(data)
 
             end = time.perf_counter()
             print(end - start)
-            print(len(easy_db.list))
+            print(len(all_data))
             pdb.set_trace()
             if self.is_last_page():
-                easy_db.commit_df()
                 return "success"
 
             self.click_next_page()
 
-            # TODO:１ページごとに繰り返す処理を追加。
-            """ TODO:１ページごとの処理時間の改善
-            まずは、manage_scrape_property_listをタスクで分解する
-            """
-
         except Exception as e:
-            print(traceback.format_exc())
+            print(e)
 
     def is_last_page(self) -> bool:
         paginations = self.find_element(
@@ -102,17 +103,20 @@ class Scraper(webdriver.Chrome):
         self.find_element(By.CSS_SELECTOR, next_button_css).click()
         self.waitng.until(lambda x: self.page_is_loaded())
 
-    def obtain_property_info(self, li: WebElement) -> list[list[str]]:
-        # まとめたことで「賃貸マンション」という余計なテキストが入っている
-        details = li.find_element(By.CSS_SELECTOR, "[class='cassetteitem_content']")
+    def obtain_property_info(self, li: Tag) -> list[list[str]]:
+        # 取得した一つの物件要素から、物件詳細を取得
+        details = li.select_one("[class='cassetteitem_content']")
         details = details.text.split("\n")
         return details
 
-    def obtain_dwelling_unit(self, li: WebElement) -> list[list[str]]:
-        dwelling_units = li.find_elements(By.CSS_SELECTOR, "[class='js-cassette_link']")
+    def obtain_dwelling_unit(self, li: Tag) -> list[list[str]]:
+        # 取得した一つの物件要素から、住戸リストを取得
+        dwelling_units = li.select("[class='js-cassette_link']")
         tbody_data = []
+        # 住戸リストから一つの要素を取得
         for tr in dwelling_units:
-            tds = tr.find_elements(By.CSS_SELECTOR, "td")
+            # 一つの住戸要素から、住戸詳細を取得
+            tds = tr.select("td")
             row_data = [td.text for td in tds]
             tbody_data.append(row_data)
 
