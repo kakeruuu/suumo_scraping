56aae3ab9e60d233fe86eb1f6f9d110904dd11e9
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-03 20:39:25 +0900

    Added functions to read all data and dictate.

--- src/model/region.py
+++ src/model/region.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Session
 
-from model.setting import Base, engine
+from model.setting import Base
 
 
 class Region(Base):
@@ -9,6 +10,9 @@ class Region(Base):
     region_id = Column(Integer, primary_key=True)
     region = Column(String, nullable=False)
 
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-if __name__ == "__main__":
-    Base.metadata.create_all(engine)
+    @classmethod
+    def read_region_all(cls, session: Session):
+        return session.query(cls).all()
