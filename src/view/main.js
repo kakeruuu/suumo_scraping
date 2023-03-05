17f844341dff0227b7fc1871834672fc3eae0a8c
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-05 14:33:51 +0900

    Module refactoring

--- src/view/main.js
+++ src/view/main.js
@@ -1,30 +1,26 @@
-function getData(url) {
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            return data;
-        })
-        .catch(error => {
-            console.error(error);
-        });
+const fetchData = async (url) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Network response was not ok.");
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-function createRegions() {
-    getData("http://localhost:8000/read_region")
-        .then(data => {
-            const selectBox = document.getElementById("region_select_box");
-            selectBox.innerHTML = `
-                        <option value="">選択してください</option>
-                        ${data.map(region => `<option value="${region.region_id}">${region.region}</option>`).join("")}
-                    `;
-        })
-        .catch(error => {
-            console.error(error);
-        })
+const createRegions = async () => {
+    const data = await fetchData("http://localhost:8000/read_region");
+    const selectBox = document.getElementById("region_select_box");
+    selectBox.innerHTML = `
+        <option value="">選択してください</option>
+        ${data.map(region => `<option value="${region.region_id}">${region.region}</option>`).join("")}
+    `;
 }
 
-
-function createPrefecturesSelectBox() {
+const createPrefecturesSelectBox = async () => {
     const regionSelectBox = document.getElementById("region_select_box");
     const prefecturesSelectBox = document.getElementById("prefecture_select_box");
     const regionId = regionSelectBox.value;
@@ -34,37 +30,29 @@ function createPrefecturesSelectBox() {
         return;
     }
 
-    getData(`http://localhost:8000/read_prefecture?region_id=${regionId}`)
-        .then(data => {
-            prefecturesSelectBox.innerHTML = `
-                        <option value="">選択してください</option>
-                        ${data.map(prefecture => `<option value="${prefecture.prefecture_id}">${prefecture.prefecture}</option>`).join("")}
-                    `;
-        })
-        .catch(error => {
-            console.error(error);
-        })
+    const data = await fetchData(`http://localhost:8000/read_prefecture?region_id=${regionId}`);
+    prefecturesSelectBox.innerHTML = `
+        <option value="">選択してください</option>
+        ${data.map(prefecture => `<option value="${prefecture.prefecture_id}">${prefecture.prefecture}</option>`).join("")}
+    `;
 }
 
-
-async function createCityCheckBoxes() {
+const createCityCheckBoxes = async () => {
     const prefecturesSelectBox = document.getElementById("prefecture_select_box");
     const prefectureId = prefecturesSelectBox.value
     const cityGroupUrl = `http://localhost:8000/read_city_group?prefecture_id=${prefectureId}`;
-    const cityGroupRes = await fetch(cityGroupUrl);
-    const cityGroupJson = await cityGroupRes.json();
+    const cityGroupJson = await fetchData(cityGroupUrl);
 
     const trs = await Promise.all(cityGroupJson.map(async (group) => {
         const cityUrl = `http://localhost:8000/read_city?group_id=${group.group_id}`;
-        const cityRes = await fetch(cityUrl);
-        const cityJson = await cityRes.json();
+        const cityJson = await fetchData(cityUrl);
 
         const cities = cityJson.map(city => `
-        <label>
-          <input type="checkbox" name="${city.city}" value="${city.city_id}">
-          ${city.city}
-        </label>
-      `).join('');
+            <label>
+                <input type="checkbox" name="${city.city}" value="${city.city_id}">
+                ${city.city}
+            </label>
+        `).join('');
 
         const citiesTd = `<td>${cities}</td>`;
         const groupTh = `<th><label><input type="checkbox" name="${group.city_group}" value="${group.group_id}">${group.city_group}</label></th>`;
@@ -76,7 +64,7 @@ async function createCityCheckBoxes() {
     tbody.innerHTML = trs.join('');
     table.appendChild(tbody);
 
-    const cityCheckBoxes = document.getElementById("city_checkboxes")
+    const cityCheckBoxes = document.getElementById("city_checkboxes");
     const existingTable = cityCheckBoxes.querySelector("table");
     if (existingTable) {
         existingTable.remove();
@@ -97,12 +85,6 @@ async function createCityCheckBoxes() {
     }
 }
 
-
-
-
-
-// TODO:thは完璧なので見た目とtdをリスト形式にする
-// TODO:グループの項目をクリックしたら、そのグループ内の市区町村全てをチェックできるようにする
-window.onload = function () {
-    createRegions()
+window.onload = async () => {
+    await createRegions();
 }
